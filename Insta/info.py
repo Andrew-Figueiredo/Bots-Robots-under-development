a1c0a00aa2afb5b8915f400fc7f7d99fc348e924
Andrew Figueiredo <andrewdw18@gmail.com>
2020-06-14 18:34:26 -0300

    Adicionando arquivo template para add conta do instagram

---
+++ Insta/info.py
@@ -0,0 +1,9 @@
+
+class info:
+    def __init__(self):
+        self.usuario = 'instagram'
+        self.senha   = 'senha_instagram'
+        self.seguir  = ['instagram1', 'instagram2','instagram3',...]
+        self.i = 10
+        self.f = 20
+
