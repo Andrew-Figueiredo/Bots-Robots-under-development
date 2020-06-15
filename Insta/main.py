8ff6e67c931091a1af698604db1ccf8b1908951e
Andrew Figueiredo <andrewdw18@gmail.com>
2020-06-15 00:06:59 -0300

    Adicionando Tela em Main e deixando o array de users Dinamico

--- Insta/main.py
+++ Insta/main.py
@@ -6,22 +6,28 @@ from time import sleep
 if __name__ == '__main__':
 
     try:
-        pessoaInfo = info()
+        # Tela Gráfica
+        tela = TelaPython()
+        tela.Iniciar()
+        users = tela.getUsers()
+        # Informações externas do usuario
+        pessoaInfo = info(users)
+        # Criação do Bot com os dados do Usuario.
         andrewbot = InstagramBot(pessoaInfo.usuario, pessoaInfo.senha)
         driver = andrewbot.getDriver()
         andrewbot.login()
         for pessoa in pessoaInfo.seguir:
-            andrewbot.seguir_pessoas_de_outra(pessoa, pessoaInfo.i, pessoaInfo.f)
+            andrewbot.seguir_pessoas_de_outra(
+                pessoa, pessoaInfo.i, pessoaInfo.f)
             andrewbot.Home()
-
 
         print("Terminou!!")
         sleep(1)
         i = 3
-        while(i>0):
-            print("Fechando em "+ i)
+        while(i > 0):
+            print("Fechando em " + i)
             sleep(2)
-            i-=1
+            i -= 1
         driver.close()
 
     except Exception as e:
